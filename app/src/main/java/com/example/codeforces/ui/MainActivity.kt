9ef6e30ac3d3d4918584b90c6dd38781f8a0a1e3
Kishan8548 <surendrakuma123m@gmail.com>
2025-10-16 22:37:43 +0530

    add runtime notification permission request for Android 13+

--- app/src/main/java/com/example/codeforces/ui/MainActivity.kt
+++ app/src/main/java/com/example/codeforces/ui/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.codeforces.ui
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.codeforces.R
 import com.example.codeforces.databinding.ActivityMainBinding
+import android.Manifest
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val NOTIFICATION_PERMISSION_CODE = 1001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +43,20 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_problem -> setCurrentFragment(problemsFragment)
             }
             true
+        }
+        Log.d("PERMISSION", "Checking notification permission...")
+        askNotificationPermission()
+    }
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val permission = Manifest.permission.POST_NOTIFICATIONS
+            if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(permission), 101)
+            } else {
+                Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, "Below Android 13 — no permission needed", Toast.LENGTH_SHORT).show()
         }
     }
 
