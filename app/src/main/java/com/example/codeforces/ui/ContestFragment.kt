80b8baece52a8aacf8b637824aa1f5788b4380ae
divyansh0655 <divyanshbhargav0308@gmail.com>
2026-04-11 15:17:30 +0530

    feat: Created Empty State for no contest found (#31)
    Co-authored-by: ThunderBlaze <68265106+Thunder-Blaze@users.noreply.github.com>

--- app/src/main/java/com/example/codeforces/ui/ContestFragment.kt
+++ app/src/main/java/com/example/codeforces/ui/ContestFragment.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.codeforces.api.RetrofitInstance
 import com.example.codeforces.databinding.FragmentContestBinding
 import com.example.codeforces.models.ApiResponse
@@ -15,6 +18,7 @@ import com.example.codeforces.models.Contest
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import android.util.Log
 
 class ContestFragment : Fragment() {
 
@@ -22,6 +26,7 @@ class ContestFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var adapter: ContestAdapter
     private val allContests = mutableListOf<Contest>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -52,6 +57,7 @@ class ContestFragment : Fragment() {
         // ✅ Step 1: Show loader, hide list
         binding.progressBarContests.visibility = View.VISIBLE
         binding.recyclerViewContests.visibility = View.GONE
+        binding.emptyView.visibility = View.GONE
 
         try {
             RetrofitInstance.api.getContestList().enqueue(object : Callback<ApiResponse<List<Contest>>> {
@@ -76,8 +82,10 @@ class ContestFragment : Fragment() {
 
                             // Show recycler view
                             binding.recyclerViewContests.visibility = View.VISIBLE
+                            binding.emptyView.visibility = View.GONE
                         } else {
-                            showError("No contests found")
+                            binding.emptyView.visibility = View.VISIBLE
+                            binding.recyclerViewContests.visibility = View.GONE
                         }
                     } else {
                         showError("Failed to load contests (Code: ${response.code()})")
@@ -87,6 +95,10 @@ class ContestFragment : Fragment() {
                 override fun onFailure(call: Call<ApiResponse<List<Contest>>>, t: Throwable) {
                     if (!isAdded || _binding == null) return
                     binding.progressBarContests.visibility = View.GONE
+
+                    binding.emptyView.visibility = View.VISIBLE
+                    binding.recyclerViewContests.visibility = View.GONE
+
                     showError("Network Error: ${t.message}")
                 }
             })
@@ -94,6 +106,10 @@ class ContestFragment : Fragment() {
             e.printStackTrace()
             if (isAdded && _binding != null) {
                 binding.progressBarContests.visibility = View.GONE
+
+                binding.emptyView.visibility = View.VISIBLE
+                binding.recyclerViewContests.visibility = View.GONE
+
                 showError("Unexpected Error: ${e.message}")
             }
         }
@@ -127,6 +143,15 @@ class ContestFragment : Fragment() {
                         Toast.makeText(requireContext(), "Showing All Contests", Toast.LENGTH_SHORT).show()
                         allContests // All
                     }
+                }
+                if(filtered.isEmpty()){
+                    binding.recyclerViewContests.visibility = View.GONE
+                    binding.emptyView.visibility = View.VISIBLE
+                }
+                else {
+                    binding.recyclerViewContests.visibility = View.VISIBLE
+                    binding.emptyView.visibility = View.GONE
+                    adapter.submitList(filtered)
                 }.sortedBy { it.startTimeSeconds }
                 adapter.submitList(filtered)
             }
